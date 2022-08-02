4e60e9ed7c3e9784638cf30259b66f861ff97e30
Alfredo <alfredofontinele08@gmail.com>
2022-08-01 21:01:44 -0300

    mobile first

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 const lista_produtos = document.getElementById("listaProdutos")
+lista_produtos.classList.add("listaProdutos")
+
 const searchInput = document.getElementById("searchInput")
 const pesquisar = document.getElementById("pesquisar")
 const totalPreco = document.getElementById("totalPreco")
@@ -6,6 +8,15 @@ const todos = document.getElementById("todos")
 const hortifruti = document.getElementById("hortifruti")
 const panificadora = document.getElementById("panificadora")
 const laticinio = document.getElementById("laticinio")
+
+const listaProdutos = document.querySelector(".listaProdutos")
+const listaCompras = document.querySelector(".carrinhoCompras")
+const carrinho_vazio = document.getElementById("carrinho-vazio")
+const total_compra = document.getElementById("total-compra")
+const preco_total = document.getElementById("preco-total")
+const quantidade_total = document.getElementById("quantidade-total")
+
+let produtosCarrinho = []
 
 const listarProdutos = (listaProdutos, secao) => {
     listaProdutos.forEach((produto) => {
@@ -16,12 +27,35 @@ const listarProdutos = (listaProdutos, secao) => {
 
 const criarCardProduto = (produto) => {
     const li = document.createElement("li")
-    li.innerHTML = `
-        <img src="${produto.img}" alt="${produto.nome}">
-        <h3>${produto.nome}</h3>
-        <span>${produto.secao}</span>
-        <p>R$ ${(produto.preco).toFixed(2)}</p>
-    `
+    li.classList.add("card")
+    const imagem = document.createElement("img")
+    const h3 = document.createElement("h3")
+    const span = document.createElement("span")
+    const ol = document.createElement("ol")
+    const p = document.createElement("p")
+    const button = document.createElement("button")
+
+    const { id, img, nome, secao, componentes, preco } = produto
+
+    imagem.src = img
+    h3.innerText = nome
+    span.innerText = secao
+
+    componentes.forEach((nutriente) => {
+        const li = document.createElement("li")
+        li.classList.add("nutriente")
+        li.innerText = nutriente
+        ol.append(li)
+    })
+
+    const div = document.createElement("div")
+    p.innerText = `R$ ${preco.toFixed(2)}`
+    button.id = id
+    button.innerText = "Comprar"
+
+    div.append(p, button)
+    li.append(imagem, h3, span, ol, div)
+
     return li
 }
 
@@ -37,7 +71,8 @@ const filtrarCategoria = (event) => {
     lista_produtos.innerHTML = ""
 
     if (localId == "todos") {
-        renderizarProdutos(PRODUTOS, lista_produtos)
+        listarProdutos(PRODUTOS, lista_produtos)
+        totalPagarPorSecao(PRODUTOS, secao_atual)
     } else {
         PRODUTOS.forEach((produto) => {
             const secao = produto.secao.toLowerCase()
@@ -45,16 +80,9 @@ const filtrarCategoria = (event) => {
                 arrayProdutos.push(produto)
             }
         })
-        renderizarProdutos(arrayProdutos, lista_produtos)
+        listarProdutos(arrayProdutos, lista_produtos)
+        totalPagarPorSecao(arrayProdutos, secao_atual)
     }
-    totalPagarPorSecao(PRODUTOS, secao_atual)
-}
-
-const renderizarProdutos = (array, secao) => {
-    array.forEach((produto) => {
-        const produtoTemplate = criarCardProduto(produto)
-        secao.append(produtoTemplate)
-    })
 }
 
 const percorrerLista = (listaProdutos, secao_atual) => {
@@ -88,7 +116,6 @@ const percorrerLista = (listaProdutos, secao_atual) => {
             }
         })
     }
-
     if (qtdProdutosEncontrados === 0) {
         lista_produtos.innerHTML = `
             <div id='resultadoPesquisa'>
@@ -100,13 +127,9 @@ const percorrerLista = (listaProdutos, secao_atual) => {
     }
 }
 
-const pesquisaPorProduto = () => {
-    percorrerLista(PRODUTOS, secao_atual)
-}
-
 const totalPagarPorSecao = (PRODUTOS, secaoAtual) => {
     if (secaoAtual == "todos") {
-        const total = PRODUTOS.map((produto) => produto.preco).reduce((acc, cur) => acc + cur)
+        const total = PRODUTOS.reduce((acc, {preco}) => acc + preco, 0)
         totalPreco.innerText = `R$ ${total.toFixed(2)}`
     } else {
         let result = 0
@@ -121,11 +144,90 @@ const totalPagarPorSecao = (PRODUTOS, secaoAtual) => {
     }
 }
 
+const quantidadeProdutos = (array) => quantidade_total.innerHTML = array.length
+
+const interceptarProduto = (event) => {
+    const localEvento = event.target
+    const idLocalEvento = localEvento.id
+    if (localEvento.tagName == "BUTTON") {
+        PRODUTOS.find( ({id, secao, nome, img, preco}) => {
+            if (id == idLocalEvento) {
+                const obj = {
+                    id: id,
+                    secao: secao,
+                    nome: nome,
+                    img: img,
+                    preco: preco
+                }
+                produtosCarrinho.push(obj)
+                criarCardsCarrinho(produtosCarrinho)
+                quantidadeProdutos(produtosCarrinho)
+                somarProdutos(produtosCarrinho)
+
+                if (produtosCarrinho.length >= 1) {
+                    total_compra.style.display = "flex"
+                    carrinho_vazio.style.display = "none"
+                }
+            }
+        })
+    }
+}
+
+const criarCardsCarrinho = (array) => {
+    listaCompras.innerHTML = ""
+    array.forEach(({img, nome, secao, preco}, index) => {
+        const li = document.createElement("li")
+        li.id = index
+        li.classList.add("cardCarrinho")
+        li.innerHTML = `
+            <div class="cardCarrinho-left">
+                <img src="${img}" alt="${nome}">
+                <div class="cardCarrinhoInfo">
+                    <h3>${nome}</h3>
+                    <span>${secao}</span>
+                    <p>R$ ${preco.toFixed(2)}</p>
+                </div>
+            </div>
+            <div class="cardCarrinho-right">
+                <img class="remover" src="./src/figma imgs/trash.png" alt="Logo | lixeira">
+            </div>
+        `
+        listaCompras.append(li)
+    })
+}
+
+const removerProduto = (event) => {
+    const localEvento = event.target
+    const id = event.target.closest("li").id
+    if (localEvento.classList.contains("remover")) {
+        if (produtosCarrinho.length <= 1) {
+            total_compra.style.display = "none"
+            carrinho_vazio.style.display = "flex"
+        }
+        produtosCarrinho.splice(id, 1)
+        criarCardsCarrinho(produtosCarrinho)
+        quantidadeProdutos(produtosCarrinho)
+        somarProdutos(produtosCarrinho)
+    }
+}
+
+const somarProdutos = array => {
+    const total = array.reduce((acc, {preco}) => acc + preco, 0)
+    preco_total.innerHTML = `R$ ${total.toFixed(2)}`.replace(",", ".")
+}
+
+const pesquisaPorProduto = () => {
+    percorrerLista(PRODUTOS, secao_atual)
+}
+
 pesquisar.addEventListener("click", pesquisaPorProduto)
 todos.addEventListener("click", filtrarCategoria)
 hortifruti.addEventListener("click", filtrarCategoria)
 panificadora.addEventListener("click", filtrarCategoria)
 laticinio.addEventListener("click", filtrarCategoria)
+
+listaProdutos.addEventListener("click", interceptarProduto)
+listaCompras.addEventListener("click", removerProduto)
 
 totalPagarPorSecao(PRODUTOS, secao_atual)
 listarProdutos(PRODUTOS, lista_produtos)
